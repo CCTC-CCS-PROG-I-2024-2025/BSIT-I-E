626247bfe5406f5e6a241428d44ca795c849b7da
Richard T. Canumay Jr. <richardcanumay96@gmail.com>
2024-11-11 14:10:19 +0800

    Update CANUMAY_ACTIVITY_IV.java

--- CANUMAY_ACTIVITY_IV.java
+++ CANUMAY_ACTIVITY_IV.java
@@ -1,26 +1,23 @@
 import java.util.Scanner;
 
-public class CANUMYAY_ACTIVITY_IV {
+public class CANUMAY_ACTIVITY_IV {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
-
         System.out.print("Enter a non-negative integer: ");
         int number = scanner.nextInt();
         
-
-        int factorial = 1;
-        
-
-        if (number >= 0) {
-
+        if (number < 0) {
+            System.out.println("Please enter a non-negative integer.");
+        } elses {
+            int factorial = 1;
+            
             for (int i = 1; i <= number; i++) {
                 factorial *= i;
             }
-
             System.out.println("Factorial of " + number + " is: " + factorial);
-        } else {
-            System.out.println("Please enter a non-negative integer.");
         }
+        
+        scanner.close();
     }
 }
