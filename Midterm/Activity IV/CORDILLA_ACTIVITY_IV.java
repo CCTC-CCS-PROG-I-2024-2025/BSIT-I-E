9af853226f5365701f8d89e58b2cca426011de07
Josh R. Cordilla <joshcordilla65@gmail.com>
2024-11-13 12:56:39 +0800

    Create CORDILLA_ACTIVITY_IV.java

---
+++ Midterm/Activity IV/CORDILLA_ACTIVITY_IV.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class CORDILLA_ACTIVITY_IV{
+    public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter a non-negative integer:");
+int number = scanner.nextInt();
+        if(number<0){
+            System.out.println("Error");
+                    }   else {
+                        int factorial = 1;
+                        for(int i=1; i<=number; i++){
+                            factorial *=i;
+                        }
+                        System.out.println(factorial);
+                    }   
+                    scanner.close();
+    }    
+}
