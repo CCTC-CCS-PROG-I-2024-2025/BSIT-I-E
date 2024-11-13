7b3e44e6cca9e7448e8dbd4ec2c8650dc632cadd
Mark Frances S. Cañete <maczkiecanete12@gmail.com>
2024-11-13 12:04:19 +0800

    Add files via upload

---
+++ Prelim/Activity IV/CANETE_ACTIVITY_IV.java
@@ -0,0 +1,17 @@
+class CANETE_ACTIVITY_IV {
+    public static void main (String[] args) {
+        int a = 5;
+        int b = 3;
+        int c = 2;
+
+        int sum = a + b;
+        int difference = b - c;
+        int product = c * a;
+        int quotient = b / a;
+
+        System.out.println(sum);
+        System.out.println(difference);
+        System.out.println(product);
+        System.out.println(quotient);
+    }
+}
