09ddf7b9eb20ba74390cc0e24b41be262d5f6737
Lawrence Paul E. Caballero <paulcaballero966@gmail.com>
2024-11-24 10:39:10 +0800

    Update CABALLERO_ACTIVITY_IV.java

--- Midterm/Activity IV/CABALLERO_ACTIVITY_IV.java
+++ Midterm/Activity IV/CABALLERO_ACTIVITY_IV.java
@@ -2,21 +2,18 @@ import java.util.Scanner;
 
 public class CABALLERO_ACTIVITY_IV {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        
-        System.out.print("Enter a non-negative integer: ");
-        int number = scanner.nextInt();
-        
-        int factorial = 1;
-        
-        if (number >= 0) {
-         
-            for (int i = 1; i <= number; i++) {
-                factorial *= i;
-            }
-            System.out.println("Factorial of " + number + " is: " + factorial);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter a non-negative integer:");
+        int number = sc.nextInt();
+
+        if (number < 0) {
+            System.out.println("Please enter a non-negative integer:");
         } else {
-            System.out.println("Please enter a non-negative integer.");
+            int factorial = 1;
+            for (int i = 1; i <= number; i++) {
+                factorial = factorial * i;
+            }
+            System.out.println(factorial);
         }
     }
 }
