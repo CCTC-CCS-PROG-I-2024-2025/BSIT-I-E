eccd191dd59a9da7116d2b27eb6fb13f0ab07c8f
Lawrence Paul E. Caballero <paulcaballero966@gmail.com>
2024-11-24 08:19:29 +0800

    Update CABALLERO_ACTIVITY_IV.java

--- Midterm/Activity IV/CABALLERO_ACTIVITY_IV.java
+++ Midterm/Activity IV/CABALLERO_ACTIVITY_IV.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class CABALLERO_ACTIVITY_VII {
+public class CABALLERO_ACTIVITY_IV {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
