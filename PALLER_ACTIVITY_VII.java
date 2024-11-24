b9f3e8ed1e847e082d1d127c0e2f3be761aeb07d
Janelle Faith Jubay Paller <janellepaller428@gmail.com>
2024-11-24 11:38:25 +0800

    Update PALLER_ACTIVITY_VII.java

--- PALLER_ACTIVITY_VII.java
+++ PALLER_ACTIVITY_VII.java
@@ -1,20 +1,22 @@
 import java.util.Scanner;
 
 public class PALLER_ACTIVITY_IV {
-    public static void main(String[] args)
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        
         System.out.print("Enter a non-negative integer: ");
         int number = scanner.nextInt();
-       
-        if (number < 0) {
-            System.out.println("Please enter a non-negative integer.");
-        } else {
-            int factorial = 1;
-
+        
+        int factorial = 1;
+        
+        if (number >= 0) {
+         
             for (int i = 1; i <= number; i++) {
                 factorial *= i;
-
-            System.out.println("The factorial of " + number + " is: " + factorial)
+            }
+            System.out.println("Factorial of " + number + " is: " + factorial);
+        } else {
+            System.out.println("Please enter a non-negative integer.");
+        }
     }
 }
