60b7a92f3189bce003b4577256e989ff99158292
Vaughn Rigel Gabutero Papellero <lumyagan345@gmail.com>
2024-11-25 11:02:25 +0800

    Add files via upload

---
+++ Prelim/Activity IV/PAPELLERO_ACTIVITY_IV.java
@@ -0,0 +1,17 @@
+class PAPELLERO_ACTIVITY_IV {
+    public static void main (String[] args) {
+        int a = 6;
+        int b = 4;
+        int c = 3;
+
+        int sum = a + b;
+        int difference = b - c;
+        int product = c * a;
+        int quotient = b / a;
+
+        System.out.println(sum);
+        System.out.println(difference);
+        System.out.println(product);
+        System.out.println(quotient);
+    }
+}
