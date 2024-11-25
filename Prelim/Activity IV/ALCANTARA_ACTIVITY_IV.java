80b9ec41667550b4d3f1fe708efd9b331350cf7b
JANCARLOTOMARONG <hs1503856@gmail.com>
2024-11-25 11:05:42 +0800

    Add files via upload

---
+++ Prelim/Activity IV/ALCANTARA_ACTIVITY_IV.java
@@ -0,0 +1,17 @@
+class ALCANTARA_ACTIVITY_IV {
+    public static void main (String[] args) {
+        int a = 6;
+        int b = 5;
+        int c = 4;
+
+        int sum = a + b;
+        int difference = b - c;
+        int product = c * a;
+        int quotient = b / a;
+
+        System.out.println(sum);
+        System.out.println(difference);
+        System.out.println(product);
+        System.out.println(quotient);
+    }
+}
