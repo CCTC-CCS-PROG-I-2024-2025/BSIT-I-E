b9ba8585c2d55cfbdea09410553ea13729a78ee4
Jean Lordy Pantonial <gumogumonogatling@gmail.com>
2024-11-29 13:21:13 +0800

    Update PANTONIAL_ACTIVITY_IV.java

--- Midterm/Activity IV/PANTONIAL_ACTIVITY_IV.java
+++ Midterm/Activity IV/PANTONIAL_ACTIVITY_IV.java
@@ -1,15 +1,9 @@
 import java.util.Scanner;
-
 class PANTONIAL_ACTIVITY_IV {
-
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
-
         System.out.println("Enter a non-negative integer: ");
-
-        int number = scanner.nextInt();
-
+    int number = scanner.nextInt();
         if (number < 0) {
             System.out.println("Error: Factorial is not defined for negative numbers.");
         } else {
