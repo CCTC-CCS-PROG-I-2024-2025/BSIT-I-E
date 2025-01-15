6d3dc1d0a300794f84550ba38bd3ad2494fbc83e
LAUREL,ADRIAN <andreylaurel02@gmail.com>
2025-01-15 14:31:50 +0800

    Create LAUREL_ACTIVITY_I.java

---
+++ PROG II/Prelim/Activities/Activity I/LAUREL_ACTIVITY_I.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class LAUREL_ACTIVITY_I_ACTIVITY_I {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] name = new String [5];
+     
+        System.out.println("ENTER YOUR NAME");
+
+        for ( int x = 0; x < name.length; x++) {
+            System.out.println("Enter name" + " : ");
+
+            name [x] = sc.nextLine();
+        }
+
+        System.out.println("YOU ENTERED THE NAME: ");
+        for (int x = 0; x < name.length; x++) {
+            System.out.println((x + 1) + ". " + name[x]);
+        }
+        
+     }
+}
