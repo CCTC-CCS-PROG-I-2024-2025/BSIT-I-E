41aecd5baa53070b5568923bcb796be076965c82
Justineallarse <dorothyeliz358@gmail.com>
2025-01-15 14:51:31 +0800

    Create ALLARSE_ACTIVITY_I.java

---
+++ PROG II/Prelim/Activities/Activity I/ALLARSE_ACTIVITY_I.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class ALLARSE_ACTIVITY_I {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] name = new String [5];
+     
+        System.out.println("ENTER THE NAME THAT YOU WOULD LIKE TO HAVE: ");
+
+        for ( int x = 0; x < name.length; x++) {
+            System.out.println("Enter name" + " : ");
+
+            name [x] = sc.nextLine();
+        }
+
+        System.out.println("You entered a name: ");
+        for (int x = 0; x < name.length; x++) {
+            System.out.println((x + 1) + ". " + name[x]);
+        }
+        
+     }
+}   
