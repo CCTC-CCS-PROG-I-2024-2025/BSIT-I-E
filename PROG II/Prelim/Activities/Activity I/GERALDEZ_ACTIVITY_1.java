bb6fd89abb12d25ef02fbcd63249b44361820736
Rex Monico Geraldez <rexgeraldez100@gmail.com>
2025-01-15 14:38:56 +0800

    Update GERALDEZ_ACTIVITY_1.java

--- PROG II/Prelim/Activities/Activity I/GERALDEZ_ACTIVITY_1.java
+++ PROG II/Prelim/Activities/Activity I/GERALDEZ_ACTIVITY_1.java
@@ -1,20 +1,32 @@
 import java.util.Scanner;
 
-public class GERALDEZ_ACTIVITY_V {
+public class DisplayNameFromArray {
     public static void main(String[] args) {
+        // Create a Scanner object for user input
         Scanner scanner = new Scanner(System.in);
 
-        String[] names = {"Mark Zuckerberg", "Elon Musk", "Bill Gates", "Jeff Bezos", "Sundar Pichai"};
+        // Create an array to store 5 names
+        String[] names = new String[5];
 
-        System.out.print("Enter a number (1-5) to select a name: ");
+        // Prompt the user to enter 5 names
+        System.out.println("Enter 5 names:");
+        for (int i = 0; i < names.length; i++) {
+            System.out.print("Name " + (i + 1) + ": ");
+            names[i] = scanner.nextLine();
+        }
+
+        // Ask the user for a number to select a name
+        System.out.println("\nChoose a number (1 to 5) to display the corresponding name:");
         int index = scanner.nextInt();
 
+        // Display the selected name
         if (index >= 1 && index <= 5) {
             System.out.println("Selected name: " + names[index - 1]);
         } else {
-            System.out.println("Invalid number. Please enter a number between 1 and 5.");
+            System.out.println("Invalid number. Please choose between 1 and 5.");
         }
 
+        // Close the scanner
         scanner.close();
     }
 }
