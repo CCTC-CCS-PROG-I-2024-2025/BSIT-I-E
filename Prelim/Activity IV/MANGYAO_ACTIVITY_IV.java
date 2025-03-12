419de6b59b627b4306f99a752ef2d772bfe60c9e
carlicemangyao123 <carlicejohnbayer@gmail.com>
2025-03-12 13:49:07 +0800

    MANGYAO_ACTIVITY_IV.java

---
+++ Prelim/Activity IV/MANGYAO_ACTIVITY_IV.java
@@ -0,0 +1,17 @@
+class MANGYAO_ACTIVITY_IV {
+    public static void main (String[] args) {
+        int a = 8;
+        int b = 5;
+        int c = 6;
+
+        int sum = a + b;
+        int difference = b - c;
+        int product = c * a;
+        int quotient = b / a;
+
+        System.out.println(sum);
+        System.out.println(difference);
+        System.out.println(product);
+        System.out.println(quotient);
+    }
+}
