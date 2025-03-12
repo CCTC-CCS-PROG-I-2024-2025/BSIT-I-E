bd85f8b30203d3ac70b32b9ed5f0cbacb51af1fa
Josh R. Cordilla <joshcordilla65@gmail.com>
2025-03-12 13:37:04 +0800

    Create CORDILLA_ACTIVITY_IV.java

---
+++ Prelim/Activity IV/CORDILLA_ACTIVITY_IV.java
@@ -0,0 +1,17 @@
+class CORDILLA_ACTIVITY_IV {
+    public static void main (String[] args) {
+        int a = 4;
+        int b = 3;
+        int c = 2;
+
+        int sum = a + b;
+        int difference = b - c;
+        int product = c * a;
+        int quotient = b / a;
+
+        System.out.println(sum);
+        System.out.println(difference);
+        System.out.println(product);
+        System.out.println(quotient);
+    }
+}
