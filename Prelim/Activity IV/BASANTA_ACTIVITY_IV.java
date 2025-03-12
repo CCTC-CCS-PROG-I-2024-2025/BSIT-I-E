7cc7824678e1e049abaa33c67ccc3f49c004e52c
keintDavid <basantakeint8@gmail.com>
2025-03-12 13:43:30 +0800

    Create BASANTA_ACTIVITY_IV.java

---
+++ Prelim/Activity IV/BASANTA_ACTIVITY_IV.java
@@ -0,0 +1,17 @@
+class BASANTA_ACTIVITY_IV {
+public static void main (String[]args) {
+    int x = 3;
+    int y = 7;
+    int z = 6;
+
+    int sum = x + z;
+    int difference = y - x;
+    int product = z * y;
+    int quotient = z / x;
+
+    System.out.println(sum);
+    System.out.println(difference);
+    System.out.println(product);
+    System.out.println(quotient);
+ }  
+}
