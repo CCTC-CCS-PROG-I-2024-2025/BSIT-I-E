5402c7950f7f88d6741df788ec2563f58414a5f8
Justin Reeven Cabaña <justinreevencabana@gmail.com>
2025-03-12 13:41:33 +0800

    Update CABAÑA_ACTIVITY_IV.java

--- Prelim/Activity IV/CABAÑA_ACTIVITY_IV.java
+++ Prelim/Activity IV/CABAÑA_ACTIVITY_IV.java
@@ -1,1 +1,18 @@
+class CABAÑA_ACTIVITY_IV {
+    public static void main (String[] args) {
+        int a = 4;
+        int b = 5;
+        int c = 6;
+
+        int sum = a + b;
+        int difference = b - c;
+        int product = c * a;
+        int quotient = b / a;
+
+        System.out.println(sum);
+        System.out.println(difference);
+        System.out.println(product);
+        System.out.println(quotient);
+    }
+}
 
