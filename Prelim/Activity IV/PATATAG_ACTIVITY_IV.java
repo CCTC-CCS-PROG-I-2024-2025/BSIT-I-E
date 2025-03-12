f44b155736196df50fed3992898b9884ba1f74cb
Vinmark Patatag <Lemonadtreeqt@gmail.com>
2025-03-12 13:50:55 +0800

    Create PATATAG_ACTIVITY_IV.java

---
+++ Prelim/Activity IV/PATATAG_ACTIVITY_IV.java
@@ -0,0 +1,17 @@
+class PATATAG_ACTIVITY_IV {
+public static void main (String[]args) {
+    int x = 2;
+    int y = 5;
+    int z = 7;
+
+    int sum = x + z;
+    int difference = y - x;
+    int product = z * y;
+    int quotient = z / x;
+
+    System.out.println(sum);
+    System.out.println(difference);
+    System.out.println(product);
+    System.out.println(quotient);
+ }  
+}
