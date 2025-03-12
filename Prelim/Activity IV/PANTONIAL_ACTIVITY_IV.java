aaa9d480b6f6528fa222bc5b77de123c4db3c09d
Jean Lordy Pantonial <gumogumonogatling@gmail.com>
2025-03-12 13:41:18 +0800

    Create PANTONIAL_ACTIVITY_IV.java

---
+++ Prelim/Activity IV/PANTONIAL_ACTIVITY_IV.java
@@ -0,0 +1,17 @@
+class PANTONIAL_ACTIVITY_IV {
+    public static void main (String[] args) {
+        int x = 6;
+        int y = 4;
+        int z = 3;
+
+        int sum = a + b;
+        int difference = b - c;
+        int product = c * a;
+        int quotient = b / a;
+
+        System.out.println(sum);
+        System.out.println(difference);
+        System.out.println(product);
+        System.out.println(quotient);
+    }
+}
