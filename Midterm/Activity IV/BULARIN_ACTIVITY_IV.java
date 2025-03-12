a27e033f2aa09c6c4eeac8d7fc9199d87b7fe17f
Ferdinan O. Bularin <ccalimquim23@gmail.com>
2025-03-12 13:56:58 +0800

    Create BULARIN_ACTIVITY_IV.java

---
+++ Midterm/Activity IV/BULARIN_ACTIVITY_IV.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class BULARIN_ACTIVITY_IV {
+
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Please enter a non-negative integer: ");
+    
+        int number = scanner.nextInt();
+
+        if(number < 0){
+            System.out.println("Please enter a non-negative integer:");
+        }   else {
+                int factorial = 1;
+              for(int i = 1; i < number; i++){
+                  factorial = factorial * i;
+                }
+                System.out.println(factorial);
+           }
+        }
+    }
