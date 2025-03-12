4e2ec2ff1e776615df6fda544bce09cf52b66a14
LAUREL,ADRIAN <andreylaurel02@gmail.com>
2025-03-12 13:37:27 +0800

    Create LAUREL_ACTIVITY_IV.java

---
+++ Prelim/Activity IV/LAUREL_ACTIVITY_IV.java
@@ -0,0 +1,17 @@
+class LAUREL_ACTIVITY_IV {
+public static void main (String[]args) {
+    int x = 8;
+    int y = 3;
+    int z = 9;
+
+    int sum = x + z;
+    int difference = y - x;
+    int product = z * y;
+    int quotient = z / x;
+
+    System.out.println(sum);
+    System.out.println(difference);
+    System.out.println(product);
+    System.out.println(quotient);
+ }  
+}
