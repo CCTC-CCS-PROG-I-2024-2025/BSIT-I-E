d87c176d626ab6144fbd8e7035ed510eafe0f214
Mark Alfie Bautista <alfiebautista216@gmail.com>
2025-03-12 14:14:16 +0800

    Create BAUTISTA_ACTIVITY_IV.java

---
+++ Prelim/Activity IV/BAUTISTA_ACTIVITY_IV.java
@@ -0,0 +1,17 @@
+class BAUTISTA_ACTIVITY_IV {
+    public static void main (String[] args) {
+        int a = 2;
+        int b = 8;
+        int c = 4;
+
+        int sum = a + b;
+        int difference = b - c;
+        int product = c * a;
+        int quotient = b / a;
+
+        System.out.println(sum);
+        System.out.println(difference);
+        System.out.println(product);
+        System.out.println(quotient);
+    }
+}
