75438f3cd17655f916477edab62f7873e405c1dd
Jay Mon Delica @didyouseethat069@gmail.com <didyouseethat069@gmail.com>
2025-03-18 16:35:43 +0800

    Create DELICA_ACTIVITY_IV.java

---
+++ Prelim/Activity IV/DELICA_ACTIVITY_IV.java
@@ -0,0 +1,17 @@
+class DELICA_ACTIVITY_IV {
+public static void main (String[]args) {
+    int x = 5;
+    int y = 3;
+    int z = 2;
+
+    int sum = x + z;
+    int difference = y - x;
+    int product = z * y;
+    int quotient = z / x;
+
+    System.out.println(sum);
+    System.out.println(difference);
+    System.out.println(product);
+    System.out.println(quotient);
+ }  
+}
