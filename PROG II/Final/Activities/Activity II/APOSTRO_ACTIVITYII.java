a8fca07c56e1ea466ad6fe07fb78f7e9669c125f
Anna Mie Apostro <annaapostro@gmail.com>
2025-03-19 14:27:53 +0800

    Add files via upload

---
+++ PROG II/Final/Activities/Activity II/APOSTRO_ACTIVITYII.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class APOSTRO_ACTIVITYII{
+
+    public static void main(String[] args) {
+        aboutMe();
+        double feet = getInput();
+        double inches = convertFeetToInches(feet);
+        displayResult(inches);
+    }
+
+     static void aboutMe() {
+        String name = "Anna Apostro";
+        int age = 19;
+        String address = "Magdugo, Toledo City";
+
+        System.out.println("My name is " + name + ", I am " + age + " years old, and I live at " + address + ".");
+    }
+
+    public static double getInput() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of feet: ");
+        return scanner.nextDouble();
+    }
+
+    public static double convertFeetToInches(double feet) {
+        return feet * 12; 
+    }
+
+    public static void displayResult(double inches) {
+        System.out.println("Inches: " + inches);
+    }
+}
+
+
+    
