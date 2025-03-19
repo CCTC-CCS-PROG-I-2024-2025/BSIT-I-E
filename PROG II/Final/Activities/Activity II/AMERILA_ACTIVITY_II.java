be141e37f0eef866ddfcc86eafe289615c7bc601
Daniel Kriester S. Amerila <ameriladanielkriester@gmail.com>
2025-03-19 14:25:33 +0800

    Create AMERILA_ACTIVITY_II.java

---
+++ PROG II/Final/Activities/Activity II/AMERILA_ACTIVITY_II.java
@@ -0,0 +1,33 @@
+mport java.util.Scanner;
+
+public class AMERILA_ACTIVITY{
+
+    public static void main(String[] args) {
+        aboutMe();
+        double feet = getInput();
+        double inches = convertFeetToInches(feet);
+        displayResult(inches);
+    }
+
+     static void aboutMe() {
+        String name = "Daniel Amerila";
+        int age = 18;
+        String address = "Cantabaco Toledo City";
+
+        System.out.println("My name is " + name + ", I am " + age + " years old, and I live at " + address + ".");
+    }
+
+    public static double getInput() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of feet: ");
+        return scanner.nextDouble();
+    }
+
+    public static double convertFeetToInches(double feet) {
+        return feet * 12; 
+    }
+
+    public static void displayResult(double inches) {
+        System.out.println("Inches: " + inches);
+    }
+}
