c2e68eb21b71c22e152e2dd7000d1fa0529f2172
Mark Alfie Bautista <alfiebautista216@gmail.com>
2025-03-19 14:25:38 +0800

    Create BAUTISTA_ACTIVITY_II.java

---
+++ PROG II/Final/Activities/Activity II/BAUTISTA_ACTIVITY_II.java
@@ -0,0 +1,15 @@
+public class Bautista
+
+    static void aboutMe(){
+        System.out.println("I am Alfie" + "I am 20" + "I live in buanoy");  
+    }    
+        public static void main(String[]args){
+
+        aboutMe();
+        
+    }
+    
+    static void convertToinches(){
+        System.out.println()
+ 
+       
