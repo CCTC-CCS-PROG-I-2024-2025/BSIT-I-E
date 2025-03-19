e21444901ebbda829928879bdd27f06a1700765a
Renthel Balejo <renthelbalejo2@gmail.com>
2025-03-19 14:34:14 +0800

    Update ALLARSE_ACTIVITY_II.java

--- PROG II/Final/Activities/Activity II/ALLARSE_ACTIVITY_II.java
+++ PROG II/Final/Activities/Activity II/ALLARSE_ACTIVITY_II.java
@@ -1,37 +1,3 @@
-import java.util.Scanner;
-
-public class ALLARSE_ACTIVITY_II {
-
-    public static void main(String[] args) {
-        aboutMe();
-        double feet = getInput();
-        double inches = convertFeetToInches(feet);
-        displayResult(inches);
-    }
-
-     static void aboutMe() {
-        String name = "Justine Allarse";
-        int age = 22;
-        String address = "Sangi, Toledo City";
-
-        System.out.println("My name is " + name + ", I am " + age + " years old, and I live at " + address + ".");
-    }
-
-    public static double getInput() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter the number of feet: ");
-        return scanner.nextDouble();
-    }
-
-    public static double convertFeetToInches(double feet) {
-        return feet * 12; 
-    }
-
-    public static void displayResult(double inches) {
-        System.out.println("Inches: " + inches);
-    }
-}
-
 
 
     
