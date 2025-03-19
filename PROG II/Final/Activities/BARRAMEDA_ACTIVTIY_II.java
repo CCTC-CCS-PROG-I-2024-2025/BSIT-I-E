04a169d11a3dfdb7b7fb1492e2cfbb177a144c4c
Vincent Lee M. Barrameda <vincentleebarrameda1@gmail.com>
2025-03-19 14:29:32 +0800

    BARRAMEDA_ACTIVITY_II

---
+++ PROG II/Final/Activities/BARRAMEDA_ACTIVTIY_II.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class BARRAMEDA_ACTIVITY {
+
+    public static void main(String[] args) {
+        aboutMe();
+        double feet = getInput();
+        double inches = convertFeetToInches(feet);
+        displayResult(inches);
+    }
+
+     static void aboutMe() {
+        String name = "Vincent Lee M.Barrameda";
+        int age = 20;
+        String address = "Luray 1, Toledo City";
+
+        System.out.println("My name is " + name + ", I am " + age + " years old, and I live at " + address + ".");
+    }
+
+    public static double getInput() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of feet: ");
+        return scanner.nextDouble();
+    }
+
+    public static double convertFeetToInches(double feet) {
+        return feet * 12; // 1 foot = 12 inches
+    }
+
+    public static void displayResult(double inches) {
+        System.out.println("Inches: " + inches);
+    }
+}
