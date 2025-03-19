87d5213385b19010cb111210c18563e74049523b
James Baliguat <jamesbaliguat1@gmail.com>
2025-03-19 14:24:40 +0800

    Create BALIGUAT_ACTIVITY_II.java

---
+++ PROG II/Final/Activities/Activity II/BALIGUAT_ACTIVITY_II.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class BALIGUAT_ACTIVITY {
+
+    public static void main(String[] args) {
+        aboutMe();
+        double feet = getInput();
+        double inches = convertFeetToInches(feet);
+        displayResult(inches);
+    }
+
+     static void aboutMe() {
+        String name = "James A. Baliguat";
+        int age = 19;
+        String address = "Poblacion, Asturias, Cebu";
+
+        System.out.println("My name is " + name + ", I am " + age + " years old, and I live at " + address + ".");
+    }
+
+    public static double getInput() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of feet: ");
+        return scanner.nextDouble();
+    }
+
+    public static double convertFeetToInches(double feet) {
+        return feet * 12; 
+    }
+
+    public static void displayResult(double inches) {
+        System.out.println("Inches: " + inches);
+    }
+}
