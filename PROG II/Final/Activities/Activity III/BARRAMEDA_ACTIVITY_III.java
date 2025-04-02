c81a03bde53d25ebffdf1e5d777bfcc8de59b1b6
Vincent Lee M. Barrameda <vincentleebarrameda1@gmail.com>
2025-04-02 14:15:57 +0800

    Create BARRAMEDA_ACTIVITY_III.java

---
+++ PROG II/Final/Activities/Activity III/BARRAMEDA_ACTIVITY_III.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+
+
+public class BARRAMEDA_ACTIVITY_III {
+
+    
+    public static double convertToFahrenheit(int celsius) {
+        return (celsius * 9.0 / 5) + 32;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        
+        System.out.print("Enter temperature in Celsius: ");
+        int celsius = scanner.nextInt();
+
+        
+        double fahrenheit = convertToFahrenheit(celsius);
+
+        
+        System.out.println("Fahrenheit: " + (int) fahrenheit);
+
+        
+        scanner.close();
+    }
+}
