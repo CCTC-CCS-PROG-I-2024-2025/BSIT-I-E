a9865619d95e6a73787635459807b1e5bba638b8
Jay Mon Delica @didyouseethat069@gmail.com <didyouseethat069@gmail.com>
2025-04-02 13:46:38 +0800

    Create DELICA_ACTIVITY_III.java

---
+++ PROG II/Final/Activities/Activity III/DELICA_ACTIVITY_III.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class DELICA_ACTIVITY_III {
+
+    // Convert Celsius to Fahrenheit
+    public static double convertToFahrenheit(int celsius) {
+        return (celsius * 9.0 / 5) + 32;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter temperature in Celsius: ");
+        int celsius = scanner.nextInt();
+
+        double fahrenheit = convertToFahrenheit(celsius);
+
+        System.out.println("Fahrenheit: " + fahrenheit);
+
+        scanner.close();
+    }
+}
