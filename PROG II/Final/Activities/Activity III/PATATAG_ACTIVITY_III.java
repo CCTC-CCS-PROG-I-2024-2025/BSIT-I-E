c0d83b7b3d3a596ffc6b034771b9a833cf0b9192
Vinmark Patatag <Lemonadtreeqt@gmail.com>
2025-04-02 13:54:00 +0800

    Create PATATAG_ACTIVITY_III.java

---
+++ PROG II/Final/Activities/Activity III/PATATAG_ACTIVITY_III.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+
+public class PATATAG_ACTIVITY_III { 
+
+    public static double celsiusToFahrenheit(double celsius) {
+        return (celsius * 9 / 5) + 32;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the temperature in Celsius: ");
+        double celsius = scanner.nextDouble();
+
+        double fahrenheit = celsiusToFahrenheit(celsius);
+
+        System.out.println("Fahrenheit: " + fahrenheit);
+
+        scanner.close();
+    }
+}
